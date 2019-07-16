375760dbfb4d85ab889a8e17a90c7eb7937c261b
Hanna <gksskcl@likelion.org>
2019-07-16 10:51:53 +0900

    first commit

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '=3#z@b0+d5!(4=+n&e9#=s+=go0vx$-v&e(x&=bs1u(m#@+304'
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
